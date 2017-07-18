0c6b77fc119bd4415bb352d3163767b37bc15bdd
Shunta Saito <shunta@preferred.jp>
2017-07-18 22:33:24 +0900

    Modify version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup(
     name='chainercmd',
-    version='1.0.0a4',
+    version='1.0.0a1',
     description='Command Line Tools for Chainer',
     author='Shunta Saito',
     author_email='shunta.saito@gmail.com',
